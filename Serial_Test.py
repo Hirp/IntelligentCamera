2edabbe94c03886daed7298d12881b349a7b5973
Hirp <hirpwastooshort@gmail.com>
2019-09-05 07:49:29 +0200

    Initial commit

---
+++ Serial_Test.py
@@ -0,0 +1,15 @@
+import time
+import serial
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM6'
+ser.open()
+while True:
+    ser.write(b'x')
+    time.sleep(1)
+    ser.write(b'90')
+    time.sleep(1)
+    ser.write(b'y')
+    time.sleep(1)
+    ser.write(b'90')
+    time.sleep(1)
