c38ca1c2088b3b5e4b2232577f125a0acb01b4f5
Dan Grønnebæk <hirpwastooshort@gmail.com>
2019-09-11 08:57:46 +0200

    Servo controls through serial connections

--- C/CamTest/CamTest.ino
+++ C/CamTest/CamTest.ino
@@ -1,12 +1,20 @@
 #include <Servo.h>
+#include <string.h>
+#include <stdlib.h>
 
 Servo servoPan;
 Servo servoTilt;
+char answerAmount[] = "";
+String answer = "";
 
 void setup() 
 {
   //Set to default position
   Serial.begin(9600);
+  while (!Serial)
+  {
+    ;
+  }
   Serial.println("Ready");
   servoPan.attach(9);
   servoTilt.attach(10);
@@ -19,31 +27,30 @@ void loop()
   while (!Serial.available())
   {
   }
-  char answer = Serial.read();
-  Serial.println(answer);
+  answer = Serial.readString();
+  Serial.print(answer);
 
-  switch (answer)
+
+  for(int i=1; i < answer.length()-1; i++)
+  {
+    answerAmount[i-1] = answer[i];
+  }
+
+  switch (answer[0])
   {
   case 'x':
-    while (!Serial.available())
-    {
-    }
-    int answerX = Serial.read();
-    Serial.println(answerX);
-    servoPan.write(answerX);
+    Serial.println('X');
+    Serial.println(answerAmount);
+    servoPan.write(atoi(answerAmount));
     break;
   case 'y':
     Serial.println('Y');
-    while (!Serial.available())
-    {
-    }
-    Serial.println('Y');
-    int answerY = Serial.read();
-    Serial.println(answerY);
-    servoTilt.write(answerY);
+    Serial.println(answerAmount);
+    servoTilt.write(atoi(answerAmount));
     break;
   default:
     Serial.println(answer);
     break;
   }
+  Serial.flush();
 }
--- Python/Serial_Test.py
+++ Python/Serial_Test.py
@@ -1,23 +1,27 @@
-import time
+from time import sleep
 import serial
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = 'COM6'
-ser.open()
+
+ser = serial.Serial('COM6',9600)  # open serial port
+print(ser.name)         # check which port was really used
+sleep(3)
+
 while True:
-    ser.write(b'x')
-    time.sleep(1)
-    ser.write(b'0')
-    time.sleep(1)
-    ser.write(b'y')
-    time.sleep(1)
-    ser.write(b'0')
-    time.sleep(1)
-    ser.write(b'x')
-    time.sleep(1)
-    ser.write(b'90')
-    time.sleep(1)
-    ser.write(b'y')
-    time.sleep(1)
-    ser.write(b'90')
-    time.sleep(1)
+    ser.write(b'x00')
+    print("x0")
+    sleep(5)
+    ser.write(b'x1800')
+    print("x180")
+    sleep(5)
+    ser.write(b'x900')
+    print("x90")
+    sleep(5)
+    ser.write(b'y00')
+    print("y0")
+    sleep(5)
+    ser.write(b'y450')
+    print("y45")
+    sleep(5)
+    ser.write(b'y900')
+    print("y90")
+    sleep(5)
+
